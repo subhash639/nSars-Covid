012b2a7ad06d2d1bb3b742fd43885969e2aa6279
Subhash Tummala <ventummala@gmail.com>
2020-04-30 19:49:55 -0700

    Initial Commit

---
+++ nSars-Covid/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  nSars-Covid
+//
+//  Created by Subhash Tummala on 4/30/20.
+//  Copyright © 2020 Subhash Tummala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ nSars-CovidTests/nSars_CovidTests.swift
@@ -0,0 +1,34 @@
+//
+//  nSars_CovidTests.swift
+//  nSars-CovidTests
+//
+//  Created by Subhash Tummala on 4/30/20.
+//  Copyright © 2020 Subhash Tummala. All rights reserved.
+//
+
+import XCTest
+@testable import nSars_Covid
+
+class nSars_CovidTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
